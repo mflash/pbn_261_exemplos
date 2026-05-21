9a9fb6e801fd45c60af6cc4a7b7b9ea26b2b7d69
Marcelo Cohen <mflashbr@gmail.com>
2026-05-21 11:00:30 -0300

    Uso de bibliotecas estáticas e dinâmicas (completo)

--- 10.Bibliotecas/main.c
+++ 10.Bibliotecas/main.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 
     drawLine(0,0, 1023, 767, (Pixel){255,0,0}, 1);
     drawRect(100, 100, 200, 150, (Pixel){0,255,0});
-    drawCircle(512, 384, 100, (Pixel){255,255,0});
+    drawCircle(512, 384, 150, (Pixel){255,127,0});
 
     saveImage(imagem, "saida.jpg");
     freeImage(imagem);
